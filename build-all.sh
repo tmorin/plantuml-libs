6e0b0eb492d17a816cc6cc7c56f149a5093c4e39
tmorin <thibault@morin.io>
2020-10-28 20:43:52 +0100

    chore: review build-all.sh script

--- build-all.sh
+++ build-all.sh
@@ -4,5 +4,6 @@ npm run build:aws-20200430 -- $@
 npm run build:aws-20200911 -- $@
 npm run build:azure-v2 -- $@
 npm run build:c4model -- $@
+npm run build:eventstorming -- $@
 npm run build:homecloud -- $@
 npm run build:material-4.0 -- $@
