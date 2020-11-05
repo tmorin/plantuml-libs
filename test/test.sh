ae2bc45f49146dae06c019a604a6901e377f8bdf
tmorin <thibault@morin.io>
2020-11-05 11:03:22 +0100

    fix: packages cannot be used remotely

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 eval "$(curl -q -s https://raw.githubusercontent.com/coryb/osht/master/osht.sh)"
 
-rm -Rf .gdiag test/wd1/*.png test/wd2/*.png
+rm -Rf .gdiag test/wd1/*.png test/wd2/*.png test/wd3/*.png
 
 RUNS bin/gdiag.js -c --wd test
 GREP "clean directory (test)"
@@ -26,6 +26,7 @@ GREP "parse directory (test)"
 GREP "process source (wd1/src1.puml)"
 NGREP "process source (wd1/src2.puml)"
 NGREP "process source (wd2/src1.puml)"
+NGREP "process source (wd3/src1.puml)"
 
 RUNS bin/gdiag.js -c --wd test/wd2
 GREP "clean directory (test/wd2)"
@@ -39,3 +40,5 @@ RUNS stat test/wd1/src1-diag1.png
 RUNS stat test/wd1/src2-diag1.png
 RUNS stat test/wd2/src1-diag1.png
 RUNS stat test/wd2/src1-diag2.png
+RUNS stat test/wd3/src1-local.png
+RUNS stat test/wd3/src1-remote.png
