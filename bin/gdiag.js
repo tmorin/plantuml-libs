344322191917e3ebba5bc90a3fafb3a0a7a89acf
tmorin <thibault@morin.io>
2022-07-15 21:55:02 +0200

    feat(aws-q3-2021): remove the package
    BREAKING CHANGE: the package is `aws-q3-2021` is removed from the distribution

--- bin/gdiag.js
+++ bin/gdiag.js
@@ -1,55 +1,59 @@
 #!/usr/bin/env node
 
-const P = require('path');
-const F = require('fs');
-const CP = require('child_process');
-const fetch = require('node-fetch')
-const moment = require('moment');
-const glob = require('glob');
+const P = require("path")
+const F = require("fs")
+const CP = require("child_process")
+const fetch = require("node-fetch")
+const moment = require("moment")
+const glob = require("glob")
 
 function getArgs() {
-    return require('yargs')
-        .scriptName('gdiag')
-        .env("GDIAG_")
-        .option('work-directory', {
-            alias: 'wd',
-            default: '.',
-            describe: 'The directory where the PlantUML files will be discovered and rendered.',
-            type: 'string'
-        })
-        .option('tech-directory', {
-            alias: 'td',
-            default: '.gdiag',
-            describe: 'The directory where technical resources will be stored.',
-            type: 'string'
-        })
-        .option('lib-directory', {
-            alias: 'ld',
-            default: P.join(__dirname, '../distribution'),
-            describe: 'The directory where the library is located.',
-            type: 'string'
-        })
-        .option('java-command', {
-            alias: 'jc',
-            default: "java",
-            describe: 'The command of the java binary.',
-            type: 'string'
-        })
-        .option('plantuml-version', {
-            alias: 'pv',
-            default: "1.2021.13",
-            describe: 'The version of PlantUML to use.',
-            type: 'string'
-        })
-        .option('clean', {
-            alias: 'c',
-            default: false,
-            describe: 'Delete recursively the pictures located in the working directory.',
-            type: 'boolean'
-        })
-        .help()
-        .usage('$0', 'Render PlantUML diagrams discovered (*.{puml,plantuml}) in the working directory.')
-        .argv;
+  return require("yargs")
+    .scriptName("gdiag")
+    .env("GDIAG_")
+    .option("work-directory", {
+      alias: "wd",
+      default: ".",
+      describe:
+        "The directory where the PlantUML files will be discovered and rendered.",
+      type: "string",
+    })
+    .option("tech-directory", {
+      alias: "td",
+      default: ".gdiag",
+      describe: "The directory where technical resources will be stored.",
+      type: "string",
+    })
+    .option("lib-directory", {
+      alias: "ld",
+      default: P.join(__dirname, "../distribution"),
+      describe: "The directory where the library is located.",
+      type: "string",
+    })
+    .option("java-command", {
+      alias: "jc",
+      default: "java",
+      describe: "The command of the java binary.",
+      type: "string",
+    })
+    .option("plantuml-version", {
+      alias: "pv",
+      default: "1.2022.6",
+      describe: "The version of PlantUML to use.",
+      type: "string",
+    })
+    .option("clean", {
+      alias: "c",
+      default: false,
+      describe:
+        "Delete recursively the pictures located in the working directory.",
+      type: "boolean",
+    })
+    .help()
+    .usage(
+      "$0",
+      "Render PlantUML diagrams discovered (*.{puml,plantuml}) in the working directory."
+    ).argv
 }
 
 /**
@@ -66,112 +70,113 @@ function getArgs() {
 /**
  * @type Args
  */
-const ARGS = getArgs();
+const ARGS = getArgs()
 
 function getLatestRunFile() {
-    return P.join(ARGS.td, 'LAST_RUN')
+  return P.join(ARGS.td, "LAST_RUN")
 }
 
 async function getLatestRun() {
-    const latestRunFile = getLatestRunFile()
-    if (F.existsSync(latestRunFile)) {
-        const stat = F.statSync(latestRunFile);
-        console.info('ran previously on (%s)', stat.mtime);
-        return moment(stat.mtime);
-    }
-    console.info('never ran previously');
+  const latestRunFile = getLatestRunFile()
+  if (F.existsSync(latestRunFile)) {
+    const stat = F.statSync(latestRunFile)
+    console.info("ran previously on (%s)", stat.mtime)
+    return moment(stat.mtime)
+  }
+  console.info("never ran previously")
 }
 
 async function setLatestRun() {
-    const latestRunFile = getLatestRunFile()
-    F.writeFileSync(latestRunFile, '');
+  const latestRunFile = getLatestRunFile()
+  F.writeFileSync(latestRunFile, "")
 }
 
 async function cleanLatestRun() {
-    const latestRunFile = getLatestRunFile()
-    if (F.existsSync(latestRunFile)) {
-        F.unlinkSync(latestRunFile);
-    }
+  const latestRunFile = getLatestRunFile()
+  if (F.existsSync(latestRunFile)) {
+    F.unlinkSync(latestRunFile)
+  }
 }
 
 async function cleanDirectory(directory, ...patterns) {
-    console.info('clean directory (%s) with (%s)', directory, patterns);
-    for (const pattern of patterns) {
-        const files = glob.sync(pattern, {
-            cwd: directory
-        });
-        for (const file of files) {
-            const filePath = P.join(directory, file);
-            console.info('delete (%s)', filePath);
-            F.unlinkSync(filePath);
-        }
+  console.info("clean directory (%s) with (%s)", directory, patterns)
+  for (const pattern of patterns) {
+    const files = glob.sync(pattern, {
+      cwd: directory,
+    })
+    for (const file of files) {
+      const filePath = P.join(directory, file)
+      console.info("delete (%s)", filePath)
+      F.unlinkSync(filePath)
     }
+  }
 }
 
 async function parseDirectory(directory, latestRun) {
-    console.info('parse directory (%s)', directory);
-    const sources = glob.sync(`**/*.{puml,plantuml}`, {
-        cwd: directory
-    });
-    for (const source of sources) {
-        const file = P.join(directory, source);
-        const stat = F.statSync(file);
-        if (!latestRun || moment(stat.mtime).isAfter(latestRun)) {
-            console.info('process source (%s)', source);
-            await renderPuml(file)
-        }
+  console.info("parse directory (%s)", directory)
+  const sources = glob.sync(`**/*.{puml,plantuml}`, {
+    cwd: directory,
+  })
+  for (const source of sources) {
+    const file = P.join(directory, source)
+    const stat = F.statSync(file)
+    if (!latestRun || moment(stat.mtime).isAfter(latestRun)) {
+      console.info("process source (%s)", source)
+      await renderPuml(file)
     }
+  }
 }
 
 function linkPlantumlLibs() {
-    const src = ARGS.ld;
-    const dst = P.join(ARGS.td, 'plantuml-libs');
-    console.info('link plantuml-libs from (%s) to (%s)', src, dst);
-    if (F.existsSync(dst)) {
-        F.unlinkSync(dst);
-    }
-    F.symlinkSync(src, dst, 'dir');
+  const src = ARGS.ld
+  const dst = P.join(ARGS.td, "plantuml-libs")
+  console.info("link plantuml-libs from (%s) to (%s)", src, dst)
+  if (F.existsSync(dst)) {
+    F.unlinkSync(dst)
+  }
+  F.symlinkSync(src, dst, "dir")
 }
 
 function getPlantumlJarPath() {
-    return P.join(ARGS.td, `plantuml-${ARGS.pv}.jar`)
+  return P.join(ARGS.td, `plantuml-${ARGS.pv}.jar`)
 }
 
 async function downloadPlantUML() {
-    const plantumlVersion = ARGS.pv
-    const plantumlJarPath = getPlantumlJarPath()
-    // https://sourceforge.net/projects/plantuml/files
-    const url = `https://sourceforge.net/projects/plantuml/files/${plantumlVersion}/plantuml.${plantumlVersion}.jar/download`;
-    F.mkdirSync(P.dirname(plantumlJarPath), {recursive: true});
-    if (!F.existsSync(plantumlJarPath)) {
-        console.info('download (%s) to (%s)', url, plantumlJarPath);
-        const resource = await fetch(url);
-        const dest = F.createWriteStream(plantumlJarPath);
-        resource.body.pipe(dest);
-        return new Promise((resolve, reject) => {
-            resource.body.on('end', resolve);
-            resource.body.on('error', reject);
-        });
-    }
+  const plantumlVersion = ARGS.pv
+  const plantumlJarPath = getPlantumlJarPath()
+  // https://sourceforge.net/projects/plantuml/files
+  // https://github.com/plantuml/plantuml/releases/download/v1.2022.6/plantuml-1.2022.6.jar
+  const url = `https://github.com/plantuml/plantuml/releases/download/v${plantumlVersion}/plantuml-${plantumlVersion}.jar`
+  F.mkdirSync(P.dirname(plantumlJarPath), { recursive: true })
+  if (!F.existsSync(plantumlJarPath)) {
+    console.info("download (%s) to (%s)", url, plantumlJarPath)
+    const resource = await fetch(url)
+    const dest = F.createWriteStream(plantumlJarPath)
+    resource.body.pipe(dest)
+    return new Promise((resolve, reject) => {
+      resource.body.on("end", resolve)
+      resource.body.on("error", reject)
+    })
+  }
 }
 
 async function renderPuml(source) {
-    const plantumlJarPath = getPlantumlJarPath()
-    CP.spawnSync(ARGS.jc, ['-jar', plantumlJarPath, source, source], {
-        stdio: 'inherit'
-    })
+  const plantumlJarPath = getPlantumlJarPath()
+  CP.spawnSync(ARGS.jc, ["-jar", plantumlJarPath, source, source], {
+    stdio: "inherit",
+  })
 }
 
 async function execute() {
-    await downloadPlantUML();
-    if (ARGS.c) {
-        await cleanLatestRun();
-        await cleanDirectory(ARGS.wd, '**/*.png');
-    }
-    linkPlantumlLibs();
-    const latestRun = await getLatestRun();
-    await parseDirectory(ARGS.wd, latestRun);
-    await setLatestRun();
+  await downloadPlantUML()
+  if (ARGS.c) {
+    await cleanLatestRun()
+    await cleanDirectory(ARGS.wd, "**/*.png")
+  }
+  linkPlantumlLibs()
+  const latestRun = await getLatestRun()
+  await parseDirectory(ARGS.wd, latestRun)
+  await setLatestRun()
 }
 
-execute().catch(e => console.error('The generation failed', e))
+execute().catch((e) => console.error("The generation failed", e))
--- test/gdiag.spec.js
+++ test/gdiag.spec.js
@@ -1,86 +1,92 @@
-const A = require('assert').strict;
-const F = require('fs');
-const P = require('path');
-const Fx = require('fs-extra').promises;
-const CP = require('child_process');
-const U = require('util');
+const A = require("assert").strict
+const F = require("fs")
+const P = require("path")
+const Fx = require("fs-extra").promises
+const CP = require("child_process")
+const U = require("util")
 
 async function cleanup() {
-    await Fx.rm('.tmp', {recursive: true, force: true})
-    await Fx.mkdir('.tmp/wd', {recursive: true})
-    await Fx.copyFile(P.join(__dirname, 'src1.puml'), '.tmp/wd/src1.puml')
-    await Fx.copyFile(P.join(__dirname, 'src2.puml'), '.tmp/wd/src2.puml')
+  await Fx.rm(".tmp", { recursive: true, force: true })
+  await Fx.mkdir(".tmp/wd", { recursive: true })
+  await Fx.copyFile(P.join(__dirname, "src1.puml"), ".tmp/wd/src1.puml")
+  await Fx.copyFile(P.join(__dirname, "src2.puml"), ".tmp/wd/src2.puml")
+  await Fx.mkdir(".tmp/td", { recursive: true })
+  await Fx.copyFile(
+    P.join(__dirname, "plantuml-1.2022.6.jar"),
+    ".tmp/td/plantuml-1.2022.6.jar"
+  )
 }
 
 async function executeGdiag(...args) {
-    CP.spawnSync(process.execPath, ['bin/gdiag.js', '--wd=.tmp/wd', '--td=.tmp/td', ...args], {
-        stdio: 'inherit'
-    })
-    await U.promisify(setTimeout)(100)
+  CP.spawnSync(
+    process.execPath,
+    ["bin/gdiag.js", "--wd=.tmp/wd", "--td=.tmp/td", ...args],
+    {
+      stdio: "inherit",
+    }
+  )
+  await U.promisify(setTimeout)(100)
 }
 
-describe('gdiag', () => {
+describe("gdiag", () => {
+  beforeEach(async function () {
+    await cleanup()
+  })
 
-    beforeEach(async function () {
-        await cleanup()
-    })
+  it("should not render missing diagrams", async function () {
+    this.timeout(100000)
 
-    it('should not render missing diagrams', async function () {
-        this.timeout(100000)
+    await executeGdiag()
+    A.equal(F.existsSync(".tmp/wd/src1-diag1.png"), true)
+    A.equal(F.existsSync(".tmp/wd/src2-diag1.png"), true)
 
-        await executeGdiag()
-        A.equal(F.existsSync('.tmp/wd/src1-diag1.png'), true)
-        A.equal(F.existsSync('.tmp/wd/src2-diag1.png'), true)
+    await Fx.rm(".tmp/wd/src1-diag1.png", { force: true })
+    await U.promisify(setTimeout)(200)
 
-        await Fx.rm('.tmp/wd/src1-diag1.png', {force: true})
-        await U.promisify(setTimeout)(200)
+    await executeGdiag()
+    A.equal(F.existsSync(".tmp/wd/src1-diag1.png"), false)
+    A.equal(F.existsSync(".tmp/wd/src2-diag1.png"), true)
+  })
 
-        await executeGdiag()
-        A.equal(F.existsSync('.tmp/wd/src1-diag1.png'), false)
-        A.equal(F.existsSync('.tmp/wd/src2-diag1.png'), true)
-    })
+  it("should render updated sources", async function () {
+    this.timeout(100000)
 
-    it('should render updated sources', async function () {
-        this.timeout(100000)
+    await executeGdiag()
+    A.equal(F.existsSync(".tmp/wd/src1-diag1.png"), true)
+    A.equal(F.existsSync(".tmp/wd/src2-diag1.png"), true)
 
-        await executeGdiag()
-        A.equal(F.existsSync('.tmp/wd/src1-diag1.png'), true)
-        A.equal(F.existsSync('.tmp/wd/src2-diag1.png'), true)
+    const src1BeforeState = await Fx.stat(".tmp/wd/src1-diag1.png")
+    const src2BeforeState = await Fx.stat(".tmp/wd/src2-diag1.png")
 
-        const src1BeforeState = await Fx.stat('.tmp/wd/src1-diag1.png');
-        const src2BeforeState = await Fx.stat('.tmp/wd/src2-diag1.png');
+    // update src1
+    await Fx.utimes(".tmp/wd/src1.puml", Date.now(), Date.now())
 
-        // update src1
-        await Fx.utimes('.tmp/wd/src1.puml', Date.now(), Date.now())
+    await executeGdiag()
 
-        await executeGdiag()
+    const src1AfterState = await Fx.stat(".tmp/wd/src1-diag1.png")
+    const src2AfterState = await Fx.stat(".tmp/wd/src2-diag1.png")
 
-        const src1AfterState = await Fx.stat('.tmp/wd/src1-diag1.png');
-        const src2AfterState = await Fx.stat('.tmp/wd/src2-diag1.png');
+    A.notEqual(src1AfterState.mtime.getTime(), src1BeforeState.mtime.getTime())
+    A.equal(src2AfterState.mtime.getTime(), src2BeforeState.mtime.getTime())
+  })
 
-        A.notEqual(src1AfterState.mtime.getTime(), src1BeforeState.mtime.getTime())
-        A.equal(src2AfterState.mtime.getTime(), src2BeforeState.mtime.getTime())
-    })
+  it("should clean before render", async function () {
+    this.timeout(100000)
 
-    it('should clean before render', async function () {
-        this.timeout(100000)
+    await executeGdiag()
+    A.equal(F.existsSync(".tmp/wd/src1-diag1.png"), true)
+    A.equal(F.existsSync(".tmp/wd/src2-diag1.png"), true)
 
-        await executeGdiag()
-        A.equal(F.existsSync('.tmp/wd/src1-diag1.png'), true)
-        A.equal(F.existsSync('.tmp/wd/src2-diag1.png'), true)
+    const src1BeforeState = await Fx.stat(".tmp/wd/src1-diag1.png")
+    const src2BeforeState = await Fx.stat(".tmp/wd/src2-diag1.png")
 
-        const src1BeforeState = await Fx.stat('.tmp/wd/src1-diag1.png');
-        const src2BeforeState = await Fx.stat('.tmp/wd/src2-diag1.png');
+    await executeGdiag("-c")
+    await U.promisify(setTimeout)(200)
 
-        await executeGdiag('-c')
-        await U.promisify(setTimeout)(200)
+    const src1AfterState = await Fx.stat(".tmp/wd/src1-diag1.png")
+    const src2AfterState = await Fx.stat(".tmp/wd/src2-diag1.png")
 
-        const src1AfterState = await Fx.stat('.tmp/wd/src1-diag1.png');
-        const src2AfterState = await Fx.stat('.tmp/wd/src2-diag1.png');
-
-        A.notEqual(src1AfterState.mtime.getTime(), src1BeforeState.mtime.getTime())
-        A.notEqual(src2AfterState.mtime.getTime(), src2BeforeState.mtime.getTime())
-    })
-
+    A.notEqual(src1AfterState.mtime.getTime(), src1BeforeState.mtime.getTime())
+    A.notEqual(src2AfterState.mtime.getTime(), src2BeforeState.mtime.getTime())
+  })
 })
-
