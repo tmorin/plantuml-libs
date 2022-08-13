66544f3a413df72b8d99d377d1193318bacab013
tmorin <thibault@morin.io>
2022-08-13 22:34:41 +0200

    feat: add the full package loader

--- scripts/generate-library.sh
+++ scripts/generate-library.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
 
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if [ -h "$SCRIPT_PATH" ]; then
+  while [ -h "$SCRIPT_PATH" ] ; do cd "$(dirname "$SCRIPT_PATH")"; SCRIPT_PATH=$(readlink "$SCRIPT_PATH"); done
+fi
+cd "$(dirname "$SCRIPT_PATH")" > /dev/null
+SCRIPT_PATH="$(pwd)";
+popd  > /dev/null
+
+REPOSITORY_PATH="$SCRIPT_PATH/..";
+
 set -ex
 
 sync
+
 uid=$(id "$USER" -u)
 gid=$(id "$USER" -g)
 
-sudo chown -R "$uid":"$gid" .workdir distribution
+mkdir -p "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
+sudo chown -R "$uid":"$gid" "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
 
-docker run --rm \
-  -v "$(pwd)/.workdir:/workdir" \
-  -v "$(pwd)/distribution:/distribution" \
-  thibaultmorin/plantuml-generator:1 \
-  plantuml-generator library generate library.yaml \
-  -c=All -O=/distribution
+cd "$REPOSITORY_PATH" && npm run generate:workdir
 
 sync
 
-sudo chown -R "$uid":"$gid" .workdir distribution
+docker run --rm \
+  -v "$REPOSITORY_PATH/.workdir:/workdir" \
+  -v "$REPOSITORY_PATH/distribution:/distribution" \
+  thibaultmorin/plantuml-generator:1 \
+  plantuml-generator library generate library.yaml \
+  -O=/distribution
+
+sync
+
+sudo chown -R "$uid":"$gid" "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
--- scripts/generate-package.sh
+++ scripts/generate-package.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if [ -h "$SCRIPT_PATH" ]; then
+  while [ -h "$SCRIPT_PATH" ] ; do cd "$(dirname "$SCRIPT_PATH")"; SCRIPT_PATH=$(readlink "$SCRIPT_PATH"); done
+fi
+cd "$(dirname "$SCRIPT_PATH")" > /dev/null
+SCRIPT_PATH="$(pwd)";
+popd  > /dev/null
+
+REPOSITORY_PATH="$SCRIPT_PATH/..";
+
 set -ex
 
 sync
@@ -7,20 +18,20 @@ sync
 uid=$(id "$USER" -u)
 gid=$(id "$USER" -g)
 
-mkdir -p .workdir distribution
-sudo chown -R "$uid":"$gid" .workdir distribution
+mkdir -p "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
+sudo chown -R "$uid":"$gid" "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
 
-npm run generate:workdir -- -p "$1"
+cd "$REPOSITORY_PATH" && npm run generate:workdir -- -p "$1"
 
 sync
 
 docker run --rm \
-  -v "$(pwd)/.workdir:/workdir" \
-  -v "$(pwd)/distribution:/distribution" \
+  -v "$REPOSITORY_PATH/.workdir:/workdir" \
+  -v "$REPOSITORY_PATH/distribution:/distribution" \
   thibaultmorin/plantuml-generator:1 \
   plantuml-generator library generate library.yaml \
   --urn="$1" --clean-urn="$1" -O=/distribution
 
 sync
 
-sudo chown -R "$uid":"$gid" .workdir distribution
+sudo chown -R "$uid":"$gid" "$REPOSITORY_PATH/.workdir" "$REPOSITORY_PATH/distribution"
