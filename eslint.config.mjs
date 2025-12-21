33871addd98ca940bd7c95e2154b5e41ac70e25e
tmorin <thibault@morin.io>
2025-12-21 13:21:46 +0100

    chore(eslint): centralize ignores in eslint.config.mjs and remove legacy config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import globals from "globals"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +15,7 @@ const compat = new FlatCompat({
 });
 
 export default [{
-    ignores: ["**/bin/", "**/test/"],
+    ignores: ["**/bin/**", "**/test/**", "**/.workdir/**", "**/distribution/**"],
 }, ...compat.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
