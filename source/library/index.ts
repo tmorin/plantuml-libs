933d3e5d09681e82fd48bd7c03227b00f6f79cf0
tmorin <thibault@morin.io>
2025-12-22 18:41:46 +0100

    feat(azure)!: upgrade to v23 icons
    BREAKING CHANGE: azure-20 is replaced by azure-23
    
    This upgrade introduces Microsoft Azure Icons v23 with 705 icons and 7 custom groups.
    The old azure-20 package is no longer available and should be replaced with azure-23 in all diagrams.

--- source/library/index.ts
+++ source/library/index.ts
@@ -1,5 +1,5 @@
 import { AwsQ32025Factory } from "./packages/aws-q3-2025"
-import { AzureV20Factory } from "./packages/azure-20"
+import { AzureV23Factory } from "./packages/azure-23"
 import { C4modelFactory } from "./packages/c4model"
 import { C4nordFactory } from "./packages/c4nord"
 import { C4K8sFactory } from "./packages/c4k8s"
@@ -18,7 +18,7 @@ import { Simpleicons14Factory } from "./packages/simpleicons-14"
 
 const PACKAGE_FACTORIES = [
   new AwsQ32025Factory(),
-  new AzureV20Factory(),
+  new AzureV23Factory(),
   new C4modelFactory(),
   new C4nordFactory(),
   new C4K8sFactory(),
